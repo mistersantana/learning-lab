53bc7c3f4d4300f0ed51643dc936bf13cb8dba61
mistersantana <elvis.santanarobles@outlook.com>
2025-10-12 18:07:39 -0400

    fuck it, I give up

---
+++ SDL3-TEST/build/CMakeFiles/sdl3_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl3_test.dir/main.c.o"
+  "CMakeFiles/sdl3_test.dir/main.c.o.d"
+  "sdl3_test"
+  "sdl3_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sdl3_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

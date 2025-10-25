cc227de1173501a48ea70043af70c85046a6a80b
mistersantana <elvis.santanarobles@outlook.com>
2025-10-25 13:10:46 -0400

    progress: week 3 session 1 coursework

--- ca-fullstack/03-Intro-JavaScript/01-Activities/02-Stu_ConsoleLog/Unsolved/script.js
+++ ca-fullstack/03-Intro-JavaScript/01-Activities/02-Stu_ConsoleLog/Unsolved/script.js
@@ -1,1 +1,4 @@
 // Write something to the console.log and look in Chrome Dev Tools
+
+console.log("Hello World")
+
--- ca-fullstack/03-Intro-JavaScript/01-Activities/06-Stu_Type_Practice/Unsolved/script.js
+++ ca-fullstack/03-Intro-JavaScript/01-Activities/06-Stu_Type_Practice/Unsolved/script.js
@@ -1,5 +1,4 @@
 // Imagine this is part of a user profile system for a web app.
-
 const userID = 64;
 const userPIN = "656302";
 const isVerified = false;
@@ -9,13 +8,13 @@ let greetingMessage = "Howdy!";
 let lastLogin;
 
 // 🧠 Insert comments to explain what each console.log() below will log to the console
-console.log(typeof userID);
-console.log(typeof userPIN);
-console.log(typeof isVerified);
-console.log(typeof accountStatus);
-console.log(typeof userScore);
-console.log(typeof greetingMessage);
-console.log(typeof lastLogin);
+console.log(typeof userID);  // this is a number
+console.log(typeof userPIN);  // this is a string
+console.log(typeof isVerified);  // this is a boolean
+console.log(typeof accountStatus);  // this is null
+console.log(typeof userScore);  // this is a number/float/double
+console.log(typeof greetingMessage);  // this is a string
+console.log(typeof lastLogin);  // this is undefined
 
 // ⏩ Variables can be reassigned to hold different types
 userScore = "Hello!";
@@ -23,6 +22,6 @@ greetingMessage = false;
 lastLogin = 23;
 
 // 🧠 Insert comments again to predict the output after reassignment
-console.log(typeof userScore);
-console.log(typeof greetingMessage);
-console.log(typeof lastLogin);
+console.log(typeof userScore);  // changed to a string
+console.log(typeof greetingMessage);  // changed to a boolean
+console.log(typeof lastLogin);  // defined as a boolean

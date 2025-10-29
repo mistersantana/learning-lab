bd77a344c11cfb0db6e78bed7e1b53c685035ef5
mistersantana <elvis.santanarobles@outlook.com>
2025-10-28 22:22:03 -0400

    wip: coursework for week 3 - js

--- ca-fullstack/03-Intro-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
+++ ca-fullstack/03-Intro-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js
@@ -1,7 +1,20 @@
 // ✅ Function Declaration — checks if two inputs are equal
+function check (a, b) {
+  if (a === b) {
+    console.log("✅ Inputs match exactly (same type and value.");
+  }else if (a == b) {
+    console.log("⚠️ Inputs match in value, but not in type.");
+  }else {
+    console.log("❌ Inputs do not match.");
+  }
+}
 
-// Logs: "✅ Inputs match exactly (same type and value)."
+// Logs: "✅ Inputs match exactly (same type and value."
+check("password123", "password123");
+
 
 // Logs: "⚠️ Inputs match in value, but not in type."
+check(42, "42");
 
 // Logs: "❌ Inputs do not match."
+check(false, "true");

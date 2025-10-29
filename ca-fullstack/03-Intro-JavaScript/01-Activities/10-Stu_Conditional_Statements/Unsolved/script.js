ce8f840a3d0e63213027f2a79394d632de6d7911
mistersantana <elvis.santanarobles@outlook.com>
2025-10-28 20:32:20 -0400

    progress: week 3 session 2 coursework

--- ca-fullstack/03-Intro-JavaScript/01-Activities/10-Stu_Conditional_Statements/Unsolved/script.js
+++ ca-fullstack/03-Intro-JavaScript/01-Activities/10-Stu_Conditional_Statements/Unsolved/script.js
@@ -7,4 +7,12 @@ const isSystemOverloaded = (systemLoad > 75);
 
 // 🧠 Your Task: Write conditional statements that evaluate combinations of true/false
 
-
+if(systemLoad >= 50 && isUserPresent) {
+  console.log("True ✅ True ✅");
+} else if(isUserPresent && isSystemOverloaded) {
+  console.log("True ✅ False ❌");
+} else if(!isUserPresent && !isSystemOverloaded) {
+  console.log("False ❌ True ✅");
+} else {
+  console.log("False ❌ False ❌");
+}
--- ca-fullstack/03-Intro-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ ca-fullstack/03-Intro-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,10 +1,15 @@
 // 💻 Create an array of at least 5 items (customize this list)
-
+let foods = ['steak', 'lasagna', 'burgers', 'chips', 'tacos'];
 
 // 1) Log the array length
-
+console.log(foods);
 
 // 2) Use a classic for loop (include the item number)
-
+for(let i = 0; i < foods.length; i++){
+  console.log(`${foods[i]} is my favorite food right now.`);
+}
 
 // 3) Use a for...of loop (no index required)
+for(let food of foods) {
+  console.log(`This food is: ${food}`);
+}
